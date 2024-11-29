1fbda0690be9e6b408c59cf1ad808e6f51ab9c0e
Jayash Saini <jayashysaini7361@gmail.com>
2024-11-30 00:46:40 +0530

    complete: finish Virox AI web app

---
+++ README.md
@@ -0,0 +1,44 @@
+# Virox
+
+🚀 Virox is an intelligent AI web app built with Next.js, designed to offer seamless, interactive conversations. Powered by advanced generative AI, Virox provides real-time responses with an intuitive, user-friendly interface. With Virox, users can ask anything and get insightful, creative, and accurate answers instantly.
+
+## 🏁 Installation
+
+1. Clone this repository to your local machine.
+2. Open the terminal in the repository directory.
+3. Install the packages:
+
+\`\`\`bash
+npm install
+\`\`\`
+
+4. Create \`.env\` file in the root folder and copy paste the content of \`.env.sample\`, and add necessary credentials.
+
+## 💻 Running
+
+- To start project in development run
+
+\`\`\`bash
+npm run dev
+\`\`\`
+
+- To start project in production run
+
+\`\`\`bash
+npm run start
+\`\`\`
+
+## How to contribute
+
+1. Fork the repository.
+2. Create a new branch for your feature or bug fix: \`git checkout -b feat/your-feature-name\` or \`git checkout -b fix/your-bug-fix-name\`.
+3. Make your changes and commit them using conventional commit messages: \`git commit -am "feat: Add new feature"\`. For more details on the commit format and other guidelines, please refer to the [Contributor Guidelines](./CONTRIBUTING.md).
+4. Push your changes to your forked repository: \`git push origin feat/your-feature-name\`.
+5. Submit a pull request to the main repository, explaining the changes you've made and providing any necessary details.
+
+## Developer Details
+
+Name - Jayash Saini <br>
+Contact No. - 9079830785 <br>
+Email ID - jayashysaini7361@gmail.com <br>
+LinkedIn - [Linked In](https://www.linkedin.com/in/jayash-saini-371bb0267/)
---
+++ src/app/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import rehypePrism from "rehype-prism";
+import "prismjs/themes/prism-okaidia.css"; // Dark theme
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.NEXT_AI_API_KEY);
+import { PlaceholdersAndVanishInput } from "@/components/placeholder";
+
+export default function Home() {
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const placeholders = [
+    "Why did the computer go to therapy?",
+    "Is AI dreaming of electric sheep or electric bills?",
+    "Who would win: AI or a toddler with a crayon?",
+    "Write a poem about why 404 errors are heartbreaking.",
+    "How can I train my toaster to do my homework?",
+  ];
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+    model
+      .generateContent([query])
+      .then(({ response }: any) =>
+        setResult(response.candidates[0].content.parts[0].text)
+      )
+      .catch((e: Error) =>
+        alert(e.message || "Error occurred while generating content!")
+      )
+      .finally(() => setIsLoading(false));
+  };
+
+  return (
+    <div className="w-full min-h-screen h-full px-4 relative ">
+      <p className="text-yellow-400 absolute top-3 right-3 ">
+        By{" "}
+        <a href="https://jayash-dev.vercel.app/" target="_blank">
+          Jayash.
+        </a>
+      </p>
+      <div className="max-w-screen-sm w-full m-auto sm:pt-52 pt-32">
+        <h2 className="mb-3 sm:mb-10 text-2xl text-center sm:text-5xl dark:text-slate-100 text-black">
+          Ask Virox Anything
+        </h2>
+        <PlaceholdersAndVanishInput
+          placeholders={placeholders}
+          onChange={handleChange}
+          onSubmit={onSubmit}
+          isLoading={isLoading}
+        />
+        {result && (
+          <div className=" sm:mt-2 mt-1 px-2  sm:px-10  sm:py-5 py-3">
+            <div className="text-slate-100  sm:text-base text-sm ">
+              <ReactMarkdown rehypePlugins={[rehypePrism]}>
+                {result}
+              </ReactMarkdown>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
